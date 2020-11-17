3a190360fbcf6bc037060abc55bbf1b166bf5726
Beatriz Miranda <beatriza.dmiranda@gmail.com>
2020-11-16 23:01:40 -0300

    fixing notas

--- code/processado/notas.py
+++ code/processado/notas.py
@@ -1,13 +1,13 @@
 import pandas as pd
 df_alunos = pd.read_csv("../dados/alunos_raiox.csv")
 
-geral = df_alunos.query("periodo_ingresso >= 2000.1").query("apv_media_geral != 'NaN'")
+geral = df_alunos.query("periodo_ingresso >= 2000.1").query("apv_media_geral != 'NaN'").query("apv_media_geral != '0.0'")
 mulheres = geral.query("sexo == 'Feminino'")
-mulheres = mulheres.groupby(['periodo_ingresso'])['apv_media_geral'].mean().astype(int)
+mulheres = mulheres.groupby(['periodo_ingresso'])['apv_media_geral'].mean().round(2)
 
 mulheres = mulheres.to_frame('media_mulheres').reset_index()
 
-geral = geral.groupby(['periodo_ingresso'])['apv_media_geral'].mean()
+geral = geral.groupby(['periodo_ingresso'])['apv_media_geral'].mean().round(2)
 geral = geral.to_frame('media_geral').reset_index()
 
 
